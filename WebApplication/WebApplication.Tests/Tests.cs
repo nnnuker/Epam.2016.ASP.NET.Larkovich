7dd080de5d970dd64ab55d9fa76e534002bede0a
Valery_Larkovich <Valery_Larkovich@EPBYMINW2230>
2016-09-05 09:11:55 +0300

    add test methods

--- WebApplication/WebApplication.Tests/Tests.cs
+++ WebApplication/WebApplication.Tests/Tests.cs
@@ -56,4 +56,45 @@ namespace WebApplication.Tests
             routeData.Values["id"].ShouldEqual("100");
         };
     }
+
+    [Subject("default routing")]
+    public class When_default_routing
+    {
+        private static Mock<HttpContextBase> httpContextMock;
+        private static RouteCollection routes;
+        private static string url = "~/";
+        private static RouteData routeData;
+
+        Establish establish = () =>
+        {
+            routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            httpContextMock = new Mock<HttpContextBase>();
+            httpContextMock.Setup(c => c.Request.AppRelativeCurrentExecutionFilePath)
+                .Returns(url);
+        };
+
+        Because of = () => { routeData = routes.GetRouteData(httpContextMock.Object); };
+
+        It should_not_be_null = () =>
+        {
+            routeData.ShouldNotBeNull();
+        };
+
+        It should_be_true = () =>
+        {
+            routeData.Values.ContainsKey("custom").ShouldBeTrue();
+        };
+
+        It should_equal_custom = () =>
+        {
+            routeData.Values["custom"].ShouldEqual("DefaultCustom");
+        };
+
+        It should_be_true_id = () =>
+        {
+            routeData.Values.ContainsKey("id").ShouldBeTrue();
+        };
+    }
 }
